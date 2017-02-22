f34c86f034f3f5e867379d94c8a3c6bae7f42ecb
Martin Rotter <rotter.martinos@gmail.com>
2017-02-22 21:03:20 +0100

    Push initial script.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # powerless
 Tiny &amp; simple pure ZSH prompt inspired by powerline.
+
+## What is this stuff?
+This is fast and tiny set of ZSH scripts providing some nice ZSH setup, including powerline-inspired prompt and some other enhancements. The prompt offers some nice visual experience and information, including Git repositories metadata.
+
+## Installation.
+Make sure that you have some powerline-enabled font installed and activated in your terminal emulator.
+
+Just copy the folder containing all the powerless scripts somewhere and then do this:
+```bash
+source "/path/to/file/powerless.zsh"
+```
+in your `.zshrc` ZSH script.
+
+Make sure that your `.zshrc` does not contain any code which might conflict with powerless scripts.
---
+++ powerless.zsh
@@ -0,0 +1,54 @@
+######################################
+### This is the ZSH powerless script.
+######################################
+### Simply source this script into
+### your .zshrc.
+### Make sure your .zshrc does not
+### contain any conflicting code.
+######################################
+
+
+###################
+### Prompt section.
+###################
+
+# Set options.
+setopt PROMPT_SUBST
+
+# Specify common variables.
+arrow_character='\ue0b0'
+newline=$'\n'
+
+get-arrow() {
+  if [[ "$#" == "2" ]]; then
+    echo -e "%F{$1}%K{$2}$arrow_character%f%k"  
+  else
+    echo -e "%F{$1}$arrow_character %f"
+  fi
+}
+
+get-date() {
+  echo -e "%F{$1}%K{$2} $(date "+%X %x") "
+}
+
+get-pwd() {
+  echo "%F{$1}%K{$2} %~ %k$(get-arrow $2)"
+}
+
+get-last-code() {
+  if [[ "$last_code" == "0" ]]; then
+    echo -e "$(get-arrow $5 $2)%F{$1}%K{$2} $last_code %f%k$(get-arrow $2 $4)"
+  else
+    echo -e "$(get-arrow $5 $3)%F{$1}%K{$3} $last_code %f%k$(get-arrow $3 $4)"
+  fi
+}
+
+precmd() {
+  last_code=$?
+
+  p_code=$(get-last-code black white red cyan yellow)
+  p_date=$(get-date black yellow)
+  p_pwd=$(get-pwd black cyan)
+}
+
+PROMPT='$p_date$p_code$p_pwd'
