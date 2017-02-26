3e4a358bb54c8df609f30053c132b262d5629094
martinrotter <rotter.martinos@gmail.com>
2017-02-26 08:15:21 +0100

    Intrudouce double prompt.

--- powerless.zsh
+++ powerless.zsh
@@ -6,8 +6,10 @@
 ### conflicting code.
 ################################################
 
-# Set options.
+# Set options and settings.
 setopt PROMPT_SUBST
+setopt PROMPT_SP
+ZLE_RPROMPT_INDENT=0
 
 # Specify colors.
 color_text="black"
@@ -17,51 +19,70 @@ color_code_wrong="red"
 color_pwd="75"
 
 # Specify common variables.
-arrow_character='\ue0b0'
+prompt_char=$'\u2023'
+arrow_character=$'\ue0b0'
+arrow_back_character=$'\uE0B2'
 newline=$'\n'
 
+alias store-colors='fg_color=$1 bg_color=$2'
+
 get-arrow() {
-  if [[ $# -eq 2 ]]; then
-    echo "%F{$1}%K{$2}$arrow_character%f%k"  
-  else
-    echo "%F{$1}$arrow_character %f"
-  fi
+  [[ $# -eq 2 ]] && echo -n "%F{$1}%K{$2}$arrow_character%f%k" || echo -n "%F{$1}$arrow_character%f"
+}
+
+get-back-arrow() {
+  [[ $# -eq 2 ]] && echo -n "%{%F{$1}%K{$2}%}$arrow_back_character%{%f%k%}" || echo -n "%{%F{$1}%}$arrow_back_character%{%f%}"
 }
 
 get-date() {
-  echo "%F{$1}%K{$2} %T %W %f%k"
+  echo -n "%{%F{$1}%K{$2}%} %T %W %{%f%k%}"
+  store-colors
 }
 
 get-pwd() {
-  echo "%F{$1}%K{$2} %~ %k$(get-arrow $2)"
+  echo -n "$(get-arrow $bg_color $2)%F{$1}%K{$2} %/ %F%k$(get-arrow $2)"
+  store-colors
+}
+
+get-user-machine() {
+  echo -n "$(get-arrow $bg_color $2)%F{$1}%K{$2} %n "
+  store-colors
 }
 
 get-last-code() {
-  if [[ $last_code -eq 0 ]]; then
-    echo "$(get-arrow $5 $2)%F{$1}%K{$2} $last_code %f%k$(get-arrow $2 $4)"
-  else
-    echo "$(get-arrow $5 $3)%F{$1}%K{$3} $last_code %f%k$(get-arrow $3 $4)"
-  fi
+  [[ $last_code -eq 0 ]] && echo -n "$(get-back-arrow $2)%{%F{$1}%K{$2}%} $last_code " || echo -n "$(get-back-arrow $3)%{%F{$1}%K{$3}%} $last_code "
 }
 
-preexec_powerless() {
+get-prompt() {
+  echo -n "$newline $prompt_char "
+}
+
+powerless-prompt() {
+  get-date $color_text $color_date
+  get-user-machine $color_text $color_code_ok
+  get-pwd $color_text $color_pwd
+  get-prompt
+}
+
+powerless-rprompt() {
+  get-last-code $color_text $color_code_ok $color_code_wrong
+}
+
+# Hook functions.
+preexec-powerless() {
   # This makes sure that every (but not first) prompt has empty line above itself.
   is_first_prompt=999
 }
 
-preexec_functions+=(preexec_powerless)
-
-precmd_powerless() {
-  last_code=$?
-  p_date=$(get-date $color_text $color_date)
-  p_code=$(get-last-code $color_text $color_code_ok $color_code_wrong $color_pwd $color_date)
-  p_pwd=$(get-pwd $color_text $color_pwd)
-  
-  if [[ $is_first_prompt -eq 999 ]]; then
-    print
-  fi
+precmd-powerless() {
+  last_code=$?  
+  [[ $is_first_prompt -eq 999 ]] && print
 }
 
-precmd_functions+=(precmd_powerless)
+# Attach the hook functions.
+preexec_functions+=(preexec-powerless)
+precmd_functions+=(precmd-powerless)
 
-PROMPT='$p_date$p_code$p_pwd'
+# Set the prompts.
+PROMPT='$(powerless-prompt)'
+RPROMPT='$(powerless-rprompt)'
