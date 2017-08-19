ec952ccf58507f32047d18462ff559ecc7df0f5a
Martin Rotter <rotter.martinos@gmail.com>
2017-08-19 19:39:33 +0200

    Tweak style, work will go on.

--- powerless.zsh
+++ powerless.zsh
@@ -9,49 +9,33 @@ ZLE_RPROMPT_INDENT=0
 
 # Specify colors.
 powerless_color_text="black"
-powerless_color_user_host="172"
+powerless_color_user_host="84"
 powerless_color_code_ok="7"
 powerless_color_code_wrong="red"
 powerless_color_pwd="75"
-powerless_color_git="114"
+powerless_color_git="202"
 
 # Specify common variables.
 prompt_char='➥'
-arrow_character=$'\ue0b0'
-newline=$'\n'
-
-store-colors() {
-  fg_color=$1 && bg_color=$2
-}
-
-get-arrow() {
-  [[ $# -eq 2 ]] && echo -n "%F{$1}%K{$2}$arrow_character%f%k" || echo -n "%F{$1}$arrow_character%f%k"
-}
 
 get-user-host() {
-  echo -n "%{%F{$1}%K{$2}%} ☻ %n@%M %{%f%k%}"
-  store-colors $1 $2
+  echo -n "%{%F{$1}%K{$2}%} %n@%M %{%f%k%}"
 }
 
 get-pwd() {
-  echo -n "$(get-arrow $bg_color $2)%F{$1}%K{$2} 📂 %~ %f%k"
-  store-colors $1 $2
+  echo -n "%{%F{$1}%K{$2}%} %~ %{%f%k%}"
 }
 
-get-git-info() {
-  git rev-parse --abbrev-ref HEAD 2> /dev/null | read git_branch
+get-git-info() { 
+  local git_branch=$(git symbolic-ref --short HEAD 2> /dev/null)
    
-  if [[ "$?" == "0" ]]; then  
-    git diff --quiet --ignore-submodules HEAD > /dev/null 2>&1
+  if [[ -n "$git_branch" ]]; then  
+    git diff --quiet --ignore-submodules --exit-code HEAD > /dev/null 2>&1
     
     [[ "$?" != "0" ]] && git_symbols="❗ "
   
-    echo -n "$(get-arrow $bg_color $2)%F{$1}%K{$2} \ue0a0 $git_branch $git_symbols%k$(get-arrow $2)%f%k"
-  else
-    echo -n "$(get-arrow $bg_color)%f%k"
+    echo -n "%{%F{$1}%K{$2}%} $git_branch $git_symbols%{%f%k%}"
   fi
-
-  store-colors $1 $2
 }
 
 get-last-code() {
@@ -59,7 +43,7 @@ get-last-code() {
 }
 
 get-prompt() {
-  echo -n "$newline" && ([[ "$(print -P "%#")" == "#" ]] && echo -n "%{%F{$powerless_color_code_wrong}%} $prompt_char%{%f%k%}\e[0m " || echo -n " $prompt_char%{\e[0m%} " )
+  echo -n "\n" && ([[ "$(print -P "%#")" == "#" ]] && echo -n "%{%F{$powerless_color_code_wrong}%} $prompt_char%{%f%k%}\e[0m " || echo -n " $prompt_char%{\e[0m%} " )
 }
 
 powerless-prompt() {
