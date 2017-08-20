c67abb49c69a6ff46fbb058fa04956a915af20cc
Martin Rotter <rotter@praktik.cz>
2017-08-20 19:59:39 +0200

    SOme changes, refactored prompt.

--- powerless.zsh
+++ powerless.zsh
@@ -8,22 +8,22 @@ setopt PROMPT_SP
 ZLE_RPROMPT_INDENT=0
 
 # Specify colors.
-powerless_color_text="black"
+powerless_color_text="0"
 powerless_color_user_host="84"
-powerless_color_code_ok="7"
-powerless_color_code_wrong="red"
+powerless_color_code_wrong="196"
 powerless_color_pwd="75"
 powerless_color_git="202"
 
 # Specify common variables.
-prompt_char='➥'
+prompt_char='$'
+rc='%{%f%k%}'
 
 get-user-host() {
-  echo -n "%{%F{$1}%K{$2}%} %n@%M %{%f%k%}"
+  echo -n "%{%F{$1}%K{$2}%} %n@%M $rc"
 }
 
 get-pwd() {
-  echo -n "%{%F{$1}%K{$2}%} %~ %{%f%k%}"
+  echo -n "%{%F{$1}%K{$2}%} %~ $rc"
 }
 
 get-git-info() { 
@@ -34,20 +34,21 @@ get-git-info() {
     
     [[ "$?" != "0" ]] && git_symbols="❗ "
   
-    echo -n "%{%F{$1}%K{$2}%} $git_branch $git_symbols%{%f%k%}"
+    echo -n "%{%F{$1}%K{$2}%} $git_branch $git_symbols$rc"
   fi
 }
 
 get-last-code() {
-  [[ $last_code -eq 0 ]] && echo -n " ✔ $last_code " || echo -n "%{%F{$3}%} ✘ $last_code %{%f%k%}"
+  [[ (-v "$last_code") && ($last_code -ne 0) ]] && echo -n "%{%F{$1}%K{$2}%} ✘ $last_code $rc"
 }
 
 get-prompt() {
-  echo -n "\n" && ([[ "$(print -P "%#")" == "#" ]] && echo -n "%{%F{$powerless_color_code_wrong}%} $prompt_char%{%f%k%}\e[0m " || echo -n " $prompt_char%{\e[0m%} " )
+  echo -n "\n" && ([[ "$(print -P "%#")" == "#" ]] && echo -n "%{%F{$powerless_color_code_wrong}%} $prompt_char$rc%{\e[0m%} " || echo -n " $prompt_char%{\e[0m%} " )
 }
 
 powerless-prompt() {
   get-user-host $powerless_color_text $powerless_color_user_host
+  get-last-code $powerless_color_text $powerless_color_code_wrong
   get-pwd $powerless_color_text $powerless_color_pwd
   get-git-info $powerless_color_text $powerless_color_git
   get-prompt
@@ -57,8 +58,7 @@ precmd-powerless() {
   last_code=$?
   
   if [[ $is_first_prompt -eq 999 ]]; then
-    printf '%*s\c' $(($(tput cols) - 4 - ${#last_code})) ""
-    print -P "$(get-last-code $powerless_color_text $powerless_color_code_ok $powerless_color_code_wrong)\n"
+    echo -n "\n"
   else
     is_first_prompt=999
   fi
